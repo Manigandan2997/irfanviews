4c0ffaefff15f28cc02258a8ed83f5fcebb9abe1
Manigandan2997 <manigandan14056@mech.ssn.edu.in>
2022-03-20 13:19:20 +0530

    array

---
+++ array.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Demo on arrays"
+#read -a n
+#echo "the value of n is" ${n[*]}
+echo "enter 6 devops tools"
+read -a tools
+echo "the devops tools are" ${tools[*]}
+echo "the first tool is" ${tools[0]}
+echo "the third  tool is" ${tools[2]}
+
